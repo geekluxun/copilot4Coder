148cc0a200ea023144f16520d3b5035cd9ffc1f9
geekluxun <geekluxun@163.com>
2024-12-21 14:33:27 +0800

    修改脚本

--- script/train.sh
+++ script/train.sh
@@ -1,8 +1,8 @@
 python  -m src.train.finetune \
   --output_dir 'tmp/output' \
-  --model_name_or_path '/Users/luxun/workspace/ai/hf/models/Qwen1.5-0.5B'  \
+  --model_name_or_path '/mnt/workspace/model/Qwen2.5-0.5B-Instruct'  \
   --use_lora True \
-  --train_data_path  '/Users/luxun/workspace/ai/ms/datasets/code_all' \
+  --train_data_path  '/mnt/workspace/data/code_all' \
   --train_data_format  'arrow' \
   --num_train_epochs  1 \
   --per_device_train_batch_size 16 \
